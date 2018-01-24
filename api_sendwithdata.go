34eb501afd24808fe9f408cc21733bc10c49996c
Facundo Médica <facundo@inmuvi.com>
2018-01-24 17:34:41 +0000

    fix sendwithdata

--- api_sendwithdata.go
+++ api_sendwithdata.go
@@ -3,7 +3,7 @@ package multichain
 func (client *Client) SendWithData(accountAddress string, assets map[string]float64, data string) (Response, error) {
 
 	msg := client.Command(
-		"sendassettoaddress",
+		"sendwithdata",
 		[]interface{}{
 			accountAddress,
 			assets,
