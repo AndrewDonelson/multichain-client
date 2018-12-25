41316441cc14916d55c325358d3ad8751196520e
Andrew Donelson <andrew@nlaak.com>
2018-12-25 11:35:09 -0600

    updated: getnewaddress

--- api_getnewaddress.go
+++ api_getnewaddress.go
@@ -1,5 +1,24 @@
 package multichain
 
+import "github.com/mitchellh/mapstructure"
+
+// GetNewAddress is a struct representing the result from the multichain.GetNewAddress() RPC Command
+type GetNewAddress struct {
+	Result struct {
+		Version string `json:"version"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+// ParseResponse takesa valid response and parses it into the model
+func (m *GetNewAddress) ParseResponse(r Response) {
+	err := mapstructure.Decode(r, &m)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func (client *Client) GetNewAddress() (Response, error) {
 
 	msg := client.Command(
