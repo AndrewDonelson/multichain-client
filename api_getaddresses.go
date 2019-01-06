427531ed69be51293371a7a581dbc6438c939102
Andrew Donelson <andrew@nlaak.com>
2019-01-06 08:26:39 -0600

    change: Address struct to represent an address

--- api_getaddresses.go
+++ api_getaddresses.go
@@ -2,20 +2,22 @@ package multichain
 
 import "github.com/mitchellh/mapstructure"
 
+type Address struct {
+	Address      string `json:"address"`
+	Ismine       bool   `json:"ismine"`
+	Iswatchonly  bool   `json:"iswatchonly"`
+	Isscript     bool   `json:"isscript"`
+	Pubkey       string `json:"pubkey"`
+	Iscompressed bool   `json:"iscompressed"`
+	Account      string `json:"account"`
+	Synchronized bool   `json:"synchronized"`
+}
+
 // GetInfo is a struct representing the result from the multichain.GetInfo() RPC Command
 type GetAddresses struct {
-	Result []struct {
-		Address      string `json:"address"`
-		Ismine       bool   `json:"ismine"`
-		Iswatchonly  bool   `json:"iswatchonly"`
-		Isscript     bool   `json:"isscript"`
-		Pubkey       string `json:"pubkey"`
-		Iscompressed bool   `json:"iscompressed"`
-		Account      string `json:"account"`
-		Synchronized bool   `json:"synchronized"`
-	} `json:"result"`
-	Error interface{} `json:"error"`
-	ID    string      `json:"id"`
+	Result []Address   `json:"result"`
+	Error  interface{} `json:"error"`
+	ID     string      `json:"id"`
 }
 
 // ParseResponse takesa valid response and parses it into the model
@@ -26,9 +28,9 @@ func (m *GetAddresses) ParseResponse(r Response) {
 	}
 }
 
-// GetAddresses returns a list of addresses in this node’s wallet. Set 
-// verbose to true to get more information about each address, formatted 
-// like the output of the validateaddress command. For more control see the 
+// GetAddresses returns a list of addresses in this node’s wallet. Set
+// verbose to true to get more information about each address, formatted
+// like the output of the validateaddress command. For more control see the
 // new listaddresses command.
 //
 // Parameters:
