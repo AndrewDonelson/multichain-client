ebcae029645a420f486dbefbc89f7b7142ae35af
Andrew Donelson <andrew@nlaak.com>
2019-02-12 22:08:37 -0600

    - added client example

---
+++ example/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/AndrewDonelson/multichain-client/example/blockchain"
+
+func main() {
+	blockchain.NewClient(
+		"GlobalWealthAndFreedom",
+		"73.55.167.87",
+		6001,
+		"gwfrpc",
+		"rpcuserpassword",
+		true,
+	)
+	blockchain.BlockchainClient.Run()
+}
